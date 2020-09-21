2857d8af7725e17d045285ceac997b25d1f83aa0
yugo_i <u15jpn@gmail.com>
2020-09-22 00:36:12 +0900

    Updates

---
+++ precache-manifest.529f8e545f69252e4dc569ffbf935d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb543ad0f9a7b9b34836c9aba1f7493",
+    "url": "/pf-react-ts-hooks-redux/index.html"
+  },
+  {
+    "revision": "118f9429bec7856dced6",
+    "url": "/pf-react-ts-hooks-redux/static/css/main.2a83f001.chunk.css"
+  },
+  {
+    "revision": "55e0a567d9ab77a9e172",
+    "url": "/pf-react-ts-hooks-redux/static/js/2.3a99e22e.chunk.js"
+  },
+  {
+    "revision": "94439a98ad244b50137649c361cb7c49",
+    "url": "/pf-react-ts-hooks-redux/static/js/2.3a99e22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118f9429bec7856dced6",
+    "url": "/pf-react-ts-hooks-redux/static/js/main.cdcf3237.chunk.js"
+  },
+  {
+    "revision": "1458075c3fbe5213be18",
+    "url": "/pf-react-ts-hooks-redux/static/js/runtime-main.ef713cf9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pf-react-ts-hooks-redux/precache-manifest.81e10fb3814733caf9e02d13e664a081.js"
+  "/pf-react-ts-hooks-redux/precache-manifest.529f8e545f69252e4dc569ffbf935d28.js"
 );
 
 self.addEventListener('message', (event) => {
